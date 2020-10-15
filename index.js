89902bf66f5bf99950e1660b38595edff925b7da
Robert <rassleppy@gmail.com>
2020-10-15 00:31:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,82 @@
 function dwarfRollCall(dwarves) {
+  var names = []
+ for(var i=0; i<dwarves.length/2; i++)
+ names.push(i+1+". "+ dwarves[i]+" " )
+ return names.join('') 
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var calls = []
+  for(var i=0; i<planeteerCalls.length; i++)
+  calls.push( planeteerCalls[i].toUpperCase()+`!`)
+  return calls
 }
+
 
 function longPlaneteerCalls(words) {
+  for(var i=0; i < words.length; i++)
+  {
+    if(words[i].length >4)
+    {
+      return true;
+    }
+  }
+  return false
+}
+  
+
+function wordsWithB(names){
+  for(var i=0; i<names.length; i++)
+  if
+
+
+
+
 }
 
-function findTheCheese (foods) {
+
+
+
+
+
+function findTheCheese(snacks) {
+  for(var i=0; i<snacks.length; i++)
+{
+  if(snacks[i]=="gouda")
+  {
+  return snacks[i]
+  }
+  if(snacks[i]=="cheddar")
+  {
+  return "cheddar"
+  }
+  if(snacks[i]=="camembert")
+  {
+  return "camembert"
+  }
+  if(snacks[i]=="swiss")
+  {
+  return "swiss"
+  }
 }
+  return "no cheese!"
+}
+
+
+  
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
